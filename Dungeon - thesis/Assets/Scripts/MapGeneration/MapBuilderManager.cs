9eab1fe1ee533c48aa2b1cd4ca2ad75955a89004
DTee <david.terins@telia.com>
2019-02-21 00:28:39 +0100

    Added basic tiles to pallette
    A dungeon is generated when pressing play

--- Dungeon - thesis/Assets/Scripts/MapGeneration/MapBuilderManager.cs
+++ Dungeon - thesis/Assets/Scripts/MapGeneration/MapBuilderManager.cs
@@ -11,12 +11,30 @@ public class MapBuilderManager : MonoBehaviour
   [SerializeField]
   Tilemap BaseTileLayer = null;
 
+  [SerializeField]
+  Tilemap CollideLayer = null;
+
 
   [SerializeField]
   UnityEngine.Tilemaps.Tile FloorTile = null;
 
   [SerializeField]
   UnityEngine.Tilemaps.Tile WallTile = null;
+
+  [SerializeField]
+  UnityEngine.Tilemaps.Tile EnemyTile = null;
+
+  [SerializeField]
+  UnityEngine.Tilemaps.Tile DoorTile = null;
+
+  [SerializeField]
+  UnityEngine.Tilemaps.Tile DoorEnterTile = null;
+
+  [SerializeField]
+  UnityEngine.Tilemaps.Tile TreasureTile = null;
+
+  [SerializeField]
+  GameObject player = null;
 
   private DungeonModel dungeon;
 
@@ -37,6 +55,8 @@ public class MapBuilderManager : MonoBehaviour
     //Gets the imported map
     dungeon = DungeonImporter.Dungeon;
 
+    BuildBaseLayer();
+
   }
 
   private void BuildBaseLayer()
@@ -44,11 +64,37 @@ public class MapBuilderManager : MonoBehaviour
     foreach (Tile tile in dungeon.InitialRoom.Tiles)
     {
 
-        BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(FloorTile));
-      if(tile.Type == TileType.WALL)
-        BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(WallTile));
 
+      switch (tile.Type)
+      {
+        case TileType.FLOOR:
+          BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(FloorTile));
+          break;
+        case TileType.WALL:
+          CollideLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(WallTile));
+          break;
+        case TileType.TREASURE:
+          BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(TreasureTile));
+          break;
+        case TileType.ENEMY:
+          BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(EnemyTile));
+          break;
+        case TileType.DOOR:
+          BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(DoorTile));
+          break;
+        case TileType.DOORENTER:
+          var doorTile = Instantiate(DoorTile);
+          BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(DoorEnterTile));
+          var s = BaseTileLayer.GetCellCenterWorld(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0));
+          Camera.main.GetComponent<SmoothCamera>().target =Instantiate(player, s , Quaternion.identity);
+          break;
+        case TileType.NONE:
+          break;
+        default:
+          break;
+      }
     }
+
     BaseTileLayer.RefreshAllTiles();
   }
 
@@ -56,10 +102,10 @@ public class MapBuilderManager : MonoBehaviour
 
   void LateUpdate()
   {
-    if (Input.GetKeyDown(KeyCode.B))
-    {
-      BuildBaseLayer();
-    }
+    //if (Input.GetKeyDown(KeyCode.B))
+    //{
+    //  BuildBaseLayer();
+    //}
   }
 
 }
---
+++ Dungeon - thesis/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+  // Normal Movements Variables
+  private float walkSpeed;
+  private float curSpeed;
+  private float maxSpeed;
+
+  //private CharacterStat plStat;
+
+  void Start()
+  {
+    //plStat = GetComponent<CharacterStat>();
+
+    walkSpeed = 2; //(float)(plStat.Speed + (plStat.Agility / 5));
+    maxSpeed = 4;// walkSpeed + (walkSpeed / 2);
+
+  }
+
+  void FixedUpdate()
+  {
+    curSpeed = walkSpeed;
+    maxSpeed = curSpeed;
+
+    // Move senteces
+    GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
+    Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+  }
+}
+
