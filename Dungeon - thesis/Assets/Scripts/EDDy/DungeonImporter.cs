5d7e3902790c1d3cdb4cb5187d4cb7f31d4f96bf
DTee <david.terins@telia.com>
2019-02-20 12:27:39 +0100

    Experimenterat med TileMaps
    Tryck på B så byggs en map från fil

---
+++ Dungeon - thesis/Assets/Scripts/EDDy/DungeonImporter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+
+public class DungeonImporter : MonoBehaviour
+{
+  [SerializeField]
+  readonly string mapFileName = "DungeonTest.map";
+
+  public DungeonModel Dungeon { get { return ImportDungeon(); } }
+
+
+  /// <summary>
+  /// Returns a Dungeon created from speciefied map/xml file
+  /// </summary>
+  /// <returns>The dungeon.</returns>
+  /// <param name="filePath">File path to map/xml file.</param>
+  private DungeonModel ImportDungeon()
+  {
+    string fullMapFilePath = Path.Combine(Application.dataPath, "Maps/"+ mapFileName);
+  
+
+    FileInfo fileInfo = new FileInfo(fullMapFilePath);
+
+    if (fileInfo.Exists)
+    {
+      DungeonModel dungeon = Serialization.DeserializeXMLFileToObject<DungeonModel>(fullMapFilePath);
+      Debug.Log("Successfully created a dungeon from: " + fileInfo.Name);
+      return dungeon; 
+    }
+    else
+    {
+      string message = "Could not find map/xml file at: " + fullMapFilePath;
+      Debug.LogWarning(message);
+      throw new Exception(message);
+    }
+  }
+}
+
+
+
---
+++ Dungeon - thesis/Assets/Scripts/MapGeneration/MapBuilderManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapBuilderManager : MonoBehaviour
+{
+  [SerializeField]
+  DungeonImporter DungeonImporter = null;
+
+  [SerializeField]
+  Tilemap BaseTileLayer = null;
+
+
+  [SerializeField]
+  UnityEngine.Tilemaps.Tile FloorTile = null;
+
+  [SerializeField]
+  UnityEngine.Tilemaps.Tile WallTile = null;
+
+  private DungeonModel dungeon;
+
+
+  private static MapBuilderManager Instance;
+  public static MapBuilderManager Singleton { get { return Instance; } }
+
+  void Awake()
+  {
+    if (Instance != null)
+    {
+      Destroy(gameObject);
+      return;
+    }
+
+    Instance = this;
+
+    //Gets the imported map
+    dungeon = DungeonImporter.Dungeon;
+
+  }
+
+  private void BuildBaseLayer()
+  {
+    foreach (Tile tile in dungeon.InitialRoom.Tiles)
+    {
+
+        BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(FloorTile));
+      if(tile.Type == TileType.WALL)
+        BaseTileLayer.SetTile(new Vector3Int((int)tile.Position.x, (int)tile.Position.y, 0), Instantiate(WallTile));
+
+    }
+    BaseTileLayer.RefreshAllTiles();
+  }
+
+
+
+  void LateUpdate()
+  {
+    if (Input.GetKeyDown(KeyCode.B))
+    {
+      BuildBaseLayer();
+    }
+  }
+
+}
